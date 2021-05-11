04b4df90106e152031566ff40337e16a9dfe2fbe
Lee JIHO <74547868+destiny3912@users.noreply.github.com>
2021-05-11 17:12:21 +0900

    Update Main.java

--- StockManagementProgram/src/Program/Main.java
+++ StockManagementProgram/src/Program/Main.java
@@ -1,9 +1,11 @@
 package Program;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("test");
 
 	}
 
